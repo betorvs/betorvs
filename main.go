82f2e8b308c5c5404bffb2546cda9c02c255659a
Roberto Scudeller <beto.rvs@gmail.com>
2022-09-21 19:07:57 +0200

    remove archived repositories

--- main.go
+++ main.go
@@ -97,7 +97,7 @@ type LatestRepository struct {
 }
 
 func main() {
-	Repositories = []string{"sonarqube-to-gitlab-webhook", "event-logger", "sensu-opsgenie-handler", "sensu-hangouts-chat-handler", "sensu-alertmanager-events", "sensu-grafana-mutator", "sensu-dynamic-check-mutator", "sensu-kubernetes-events", "secretreceiver", "secretpublisher"}
+	Repositories = []string{"sonarqube-to-gitlab-webhook", "event-logger", "secretreceiver", "secretpublisher"}
 	var repos []string
 	for _, v := range Repositories {
 		github := fmt.Sprintf("%s/%s/releases/latest", Github, v)
